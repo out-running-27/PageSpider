da7e6d37a5a5a9d1b0224037dbc1636938df074e
out-running-27 <paul.russum@gmail.com>
2018-10-02 07:27:39 -0500

    reformatted files

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,13 +1,13 @@
 def load_urls_from_file(file_path: srt):
-    #TODO: add the code needed to read the text file with urls in it
+    # TODO: add the code needed to read the text file with urls in it
     pass
 
 
 def load_page(url: str):
-    #TODO: add the code that readsd the url
+    # TODO: add the code that reads the url
     pass
 
 
 def scrape_page(page_contents: str):
-    #TODO: analyze the text
+    # TODO: analyze the text
     pass
